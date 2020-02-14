218c9561f6f64bf5e33220647905edeeb8f00054
Danielo Rodriguez <rdanielo@gmail.com>
2020-02-14 12:32:42 +0100

    feat: calculate in hours for month

--- front/src/features/stats/createChartData.js
+++ front/src/features/stats/createChartData.js
@@ -9,13 +9,14 @@ import startOfMonth from 'date-fns/startOfMonth';
 import isWithinInterval from 'date-fns/isWithinInterval';
 import format from 'date-fns/fp/format';
 import differenceInMinutes from 'date-fns/differenceInMinutes';
+import differenceInHours from 'date-fns/differenceInHours';
 /**
  * 
  * @param {Function} formatter should format the date to string
  * @returns {[date]: { [name]: Number} } hashmap of name-value indexed by formatted date
  */
-export const addToRow = formatter => (map, { name, startDate, endDate }) => {
-    const duration = differenceInMinutes(new Date(endDate || Date.now()), new Date(startDate));
+export const addToRow = (formatter, diffCalc) => (map, { name, startDate, endDate }) => {
+    const duration = diffCalc(new Date(endDate || Date.now()), new Date(startDate));
     const date = formatter(new Date(startDate));
     const existing = (map[date] || {})
     const names = map.names || new Set()
@@ -31,11 +32,14 @@ export const addToRow = formatter => (map, { name, startDate, endDate }) => {
     };
 };
 
+window.hoursdiff = differenceInHours;
+const diffInHours = (a,b) => (differenceInMinutes(a, b) / 60);
+const makeDayRow = addToRow(format('HH:mm'), differenceInMinutes);
+const makeWeekRow = addToRow(format('E do MMM'), differenceInMinutes);
+const makeMonthRow = addToRow(format('Io'), diffInHours);
 
-const makeDayRow = addToRow(format('HH:mm'));
-const makeWeekRow = addToRow(format('E do MMM'));
+const omitNamesProp = ({ names, ...rest }) => rest
 
-const omitNamesProp = ({names, ...rest}) => rest
 
 export function createChartData({ daysAgo, weeksAgo = 0, monthsAgo = 0, sessions }) {
     const today = endOfDay(new Date());
@@ -54,7 +58,7 @@ export function createChartData({ daysAgo, weeksAgo = 0, monthsAgo = 0, sessions
             const d = new Date(session.startDate)
             if (isWithinInterval(d, dayInterval)) acc.d = makeDayRow(acc.d, session)
             if (isWithinInterval(d, weekInterval)) acc.w = makeWeekRow(acc.w, session)
-            if (isWithinInterval(d, monthInterval)) acc.m = makeWeekRow(acc.m, session)
+            if (isWithinInterval(d, monthInterval)) acc.m = makeMonthRow(acc.m, session)
             return acc; // I don't usually mutate, but this is a big performance gain on this case
         },
         { d: { names: new Set() }, w: { names: new Set() }, m: { names: new Set() } }) // I was originally using longer names, but I think this is obvious
