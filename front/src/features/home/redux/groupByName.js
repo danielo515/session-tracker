9c36ec151369b8552d637e606c649c149f8e0128
Danielo Rodriguez <rdanielo@gmail.com>
2020-02-17 11:31:08 +0100

    feat: include the lastRun in formatted sessions

--- front/src/features/home/redux/groupByName.js
+++ front/src/features/home/redux/groupByName.js
@@ -1,6 +1,6 @@
-export const formatGroupedSessions = (acc = { nested: [] }, session) => ({
+export const formatGroupedSessions = (acc = { nested: [], lastRun:"" }, session) => ({
     name: session.name.trim(),
-    lastRun: "",
+    lastRun: acc.lastRun < session.endDate ? session.endDate : acc.lastRun, 
     nested: [...acc.nested, session]
 })
 
