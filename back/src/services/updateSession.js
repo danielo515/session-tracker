d3ce51c37b2c7bd1d15b4261ce195534ff1a36e6
Danielo Rodriguez <rdanielo@gmail.com>
2020-03-19 11:07:10 +0100

    feat(back): allow to edit start end end dates

--- back/src/services/updateSession.js
+++ back/src/services/updateSession.js
@@ -1,18 +1,21 @@
-const updateSession = ({ dynamoDb }) => async ({ id, name, endDate, userId }) => {
+const updateSession = ({ dynamoDb }) => async ({ id, name, endDate, userId, startDate }) => {
   const timestamp = new Date().getTime();
-  const date = new Date(endDate);
+  const eDate = new Date(endDate);
+  const sDate = new Date(startDate);
   const params = {
     TableName: process.env.DYNAMODB_TABLE,
     Key: { id, userId },
     ...(name && { ExpressionAttributeNames: { '#name': 'name' } }),
     ExpressionAttributeValues: {
       ...(name && { ':name': name }),
-      ...(endDate && { ':endDate': endDate && date.toISOString() }),
+      ...(endDate && { ':endDate': endDate && eDate.toISOString() }),
+      ...(startDate && { ':startDate': startDate && sDate.toISOString() }),
       ':updatedAt': timestamp,
     },
     UpdateExpression: `SET ${[
       name && '#name = :name',
       endDate && 'endDate = :endDate',
+      startDate && 'startDate = :startDate',
       'updatedAt = :updatedAt'].filter((x) => x).join(', ')}`,
     ReturnValues: 'ALL_NEW',
   };
--- back/src/sessions/updateSession.js
+++ back/src/sessions/updateSession.js
@@ -1,15 +1,23 @@
+const makeHandler = require("../setup/makeHandler");
 
-const makeHandler = require('../setup/makeHandler');
-
-const updateSessionHandler = ({ updateSession }) => (
-  async ({ body: session, auth: user, params }) => {
-    const { name, endDate } = session;
-    return {
-      status: 200,
-      result: {
-        session: await updateSession({ id: params.id, name, endDate, userId: user.userId }),
-      },
-    };
-  });
+const updateSessionHandler = ({ updateSession }) => async ({
+  body: session,
+  auth: user,
+  params,
+}) => {
+  const { name, endDate, startDate } = session;
+  return {
+    status: 200,
+    result: {
+      session: await updateSession({
+        id: params.id,
+        name,
+        endDate,
+        startDate,
+        userId: user.userId,
+      }),
+    },
+  };
+};
 
 module.exports.handler = makeHandler(updateSessionHandler);
