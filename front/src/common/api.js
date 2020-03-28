4f1c6c29a035c1cfac5bdd42eb68ce143f4bd482
Danielo Rodriguez <rdanielo@gmail.com>
2020-03-28 18:56:36 +0100

    chore: lints

--- front/src/common/api.js
+++ front/src/common/api.js
@@ -3,83 +3,83 @@ import axios from 'axios';
 import { isCloseToExpire } from './isTokenExpired';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
-  useAuth: true,
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+    useAuth: true,
 });
 
 const refreshToken = fn => async args => {
-  if (!args.token || !isCloseToExpire(args.token)) return fn(args);
-  const {
-    data: { token },
-  } = await api
-    .post('refreshToken', {}, { headers: { Authorization: `Bearer ${args.token}` } })
-    .catch(() => fn(args));
-  localStorage.setItem('token', token);
-  return fn({ token, ...args });
+    if (!args.token || !isCloseToExpire(args.token)) return fn(args);
+    const {
+        data: { token },
+    } = await api
+        .post('refreshToken', {}, { headers: { Authorization: `Bearer ${args.token}` } })
+        .catch(() => fn(args));
+    localStorage.setItem('token', token);
+    return fn({ token, ...args });
 };
 
 const formatError = ({ response, request }) => ({
-  error: { data: response.data, status: request.status },
+    error: { data: response.data, status: request.status },
 });
 const formatResponse = ({ data }) => ({ response: data });
 
 export const login = ({ email, password }) =>
-  api
-    .post('login', { email, password })
-    .then(formatResponse)
-    .catch(formatError);
+    api
+        .post('login', { email, password })
+        .then(formatResponse)
+        .catch(formatError);
 
 export const signUp = ({ email, password, name }) =>
-  api
-    .post('users', { email, password, name })
-    .then(formatResponse)
-    .catch(formatError);
+    api
+        .post('users', { email, password, name })
+        .then(formatResponse)
+        .catch(formatError);
 
 export const me = ({ token }) =>
-  api
-    .get('me', { headers: { Authorization: `Bearer ${token}` } })
-    .then(formatResponse)
-    .catch(formatError);
+    api
+        .get('me', { headers: { Authorization: `Bearer ${token}` } })
+        .then(formatResponse)
+        .catch(formatError);
 
 export const listSessions = refreshToken(({ token }) =>
-  api
-    .get('/sessions', { headers: { Authorization: `Bearer ${token}` } })
-    .then(formatResponse)
-    .catch(formatError),
+    api
+        .get('/sessions', { headers: { Authorization: `Bearer ${token}` } })
+        .then(formatResponse)
+        .catch(formatError),
 );
 
 export const startSession = ({ token, name }) =>
-  api
-    .post(
-      '/sessions',
-      { name, startDate: new Date().toISOString() },
-      { headers: { Authorization: `Bearer ${token}` } },
-    )
-    .then(formatResponse)
-    .catch(formatError);
+    api
+        .post(
+            '/sessions',
+            { name, startDate: new Date().toISOString() },
+            { headers: { Authorization: `Bearer ${token}` } },
+        )
+        .then(formatResponse)
+        .catch(formatError);
 
 export const stopSession = ({ token, id, name }) =>
-  api
-    .patch(
-      `/sessions/${id}`,
-      { name, endDate: new Date().toISOString() },
-      { headers: { Authorization: `Bearer ${token}` } },
-    )
-    .then(formatResponse)
-    .catch(formatError);
+    api
+        .patch(
+            `/sessions/${id}`,
+            { name, endDate: new Date().toISOString() },
+            { headers: { Authorization: `Bearer ${token}` } },
+        )
+        .then(formatResponse)
+        .catch(formatError);
 
 export const updateSession = ({ token, id, name, startDate, endDate }) =>
-  api
-    .patch(
-      `/sessions/${id}`,
-      { name, endDate, startDate },
-      { headers: { Authorization: `Bearer ${token}` } },
-    )
-    .then(formatResponse)
-    .catch(formatError);
+    api
+        .patch(
+            `/sessions/${id}`,
+            { name, endDate, startDate },
+            { headers: { Authorization: `Bearer ${token}` } },
+        )
+        .then(formatResponse)
+        .catch(formatError);
 
 export const deleteSession = ({ token, id }) =>
-  api
-    .delete(`/sessions/${id}`, { headers: { Authorization: `Bearer ${token}` } })
-    .then(formatResponse)
-    .catch(formatError);
+    api
+        .delete(`/sessions/${id}`, { headers: { Authorization: `Bearer ${token}` } })
+        .then(formatResponse)
+        .catch(formatError);
