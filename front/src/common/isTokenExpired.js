6449fbcbb4482f3e19e6b35f935729e7a7d15fbf
Danielo Rodriguez <rdanielo@gmail.com>
2020-04-07 16:34:24 +0200

    fix: date round bug

--- front/src/common/isTokenExpired.js
+++ front/src/common/isTokenExpired.js
@@ -8,11 +8,14 @@ const parseTokenInfo = token => {
 
 export const isTokenExpired = token => {
   const tokenInfo = parseTokenInfo(token);
-  return tokenInfo.exp < ((Date.now() / 100) | 0);
+  return tokenInfo.exp < Date.now() / 100;
 };
 
+/*
+ * token.exp is in seconds
+ */
 export const isCloseToExpire = token => {
   const tokenInfo = parseTokenInfo(token);
-  const timeDiff = tokenInfo.exp - ((Date.now() / 100) | 0);
+  const timeDiff = tokenInfo.exp - Date.now() / 100;
   return timeDiff < 4 * oneHour;
 };
