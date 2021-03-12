9f7c95019342da8a55aa8b470b3bbef364859c92
Danielo Rodriguez <rdanielo@gmail.com>
2021-03-12 08:04:05 +0100

    chore: rund on minutes4Human

--- front/src/formatters/formatMinutes4Human.js
+++ front/src/formatters/formatMinutes4Human.js
@@ -1,8 +1,8 @@
 /** @param {number} minutes */
 export const formatMinutes4Human = minutes => {
-  if (minutes < 60) return `${minutes} minutes`;
+  if (minutes < 60) return `${Math.round(minutes)} minutes`;
   const hours = (minutes / 60) | 0;
-  const remaining = minutes % 60;
+  const remaining = Math.round(minutes % 60);
   const hourStr = hours > 1 ? 'hours' : 'hour';
   return `${hours} ${hourStr} ${remaining} min`;
 };
