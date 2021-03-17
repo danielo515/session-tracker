784f2c10dcae4dc5f9bcec96855cdd7ad965ed49
Danielo Rodriguez <rdanielo@gmail.com>
2021-03-17 07:35:48 +0100

    chore: some docs

--- front/src/features/home/msToHourMinSec.js
+++ front/src/features/home/msToHourMinSec.js
@@ -2,13 +2,17 @@ export const second = 1000;
 export const minute = 60 * second;
 export const hour = minute * 60;
 
+/**
+ * turns a value in milliseconds into hours, minutes and seconds
+ * @param {number} ms milisseconds to convert
+ */
 export const msToHourMinSec = ms => {
-  const hours = ms / hour | 0;
-  const minutes = (ms % hour) / minute | 0; // 32 bit integer shorthand
-  const seconds = (ms % minute) / second | 0;
+  const hours = (ms / hour) | 0;
+  const minutes = ((ms % hour) / minute) | 0; // 32 bit integer shorthand
+  const seconds = ((ms % minute) / second) | 0;
   return [
     `${hours}`.padStart(2, '0'),
     `${minutes}`.padStart(2, '0'),
-    `${seconds}`.padStart(2, '0')
+    `${seconds}`.padStart(2, '0'),
   ];
 };
