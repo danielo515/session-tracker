df9d35e711b2e084d23cea1f9c90812419b989dd
Danielo Rodriguez <rdanielo@gmail.com>
2021-08-11 17:19:51 +0200

    feat: formatMinutes4Human allows short version

--- front/src/formatters/formatMinutes4Human.js
+++ front/src/formatters/formatMinutes4Human.js
@@ -1,8 +1,8 @@
 /** @param {number} minutes */
-export const formatMinutes4Human = minutes => {
+export const formatMinutes4Human = (minutes, short = true) => {
   if (minutes < 60) return `${Math.round(minutes)} minutes`;
   const hours = (minutes / 60) | 0;
   const remaining = Math.round(minutes % 60);
-  const hourStr = hours > 1 ? 'hours' : 'hour';
-  return `${hours} ${hourStr} ${remaining} min`;
+  const hourStr = short ? 'h' : hours > 1 ? 'hours' : 'hour';
+  return `${hours} ${hourStr} ${remaining} ${short ? 'm' : 'min'}`;
 };
