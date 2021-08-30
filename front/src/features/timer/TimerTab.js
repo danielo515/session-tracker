c5ec2fff52c1d202afb75545e2962686d84cc421
Danielo Rodriguez <rdanielo@gmail.com>
2021-08-30 17:10:03 +0200

    feat: display circular timer fix: buttons on card fix: add30

--- front/src/features/timer/TimerTab.js
+++ front/src/features/timer/TimerTab.js
@@ -1,47 +1,21 @@
+import { CircularTimer } from './CircularTimer';
 import React from 'react';
 import Box from '@material-ui/core/Box';
-import { RenderTimer } from '../home/Timer';
-
 import Page from '../common/Page';
-// import PropTypes from 'prop-types';
-import {} from './redux/hooks';
 import { useEditRunningSession } from 'features/common/redux/editRunningSession';
-import { Button, Card, Grid, IconButton, Typography } from '@material-ui/core';
+import { Button, Card, Grid, Typography } from '@material-ui/core';
 import useRunningSessionStats from './useRunningSessionStats';
 import { msToHuman } from 'formatters/formatDateDiff';
-import format from 'date-fns/format';
-import EditIcon from '@material-ui/icons/Edit';
 import StopIcon from '@material-ui/icons/Stop';
 // import FastRewindIcon from '@material-ui/icons/FastRewind';
 import { subMinutes } from 'date-fns';
 import { Replay, Add, Remove } from '@material-ui/icons';
 import { useStopRunningSession } from 'features/common/redux/useStopRunningSession';
 import { useInterval } from '@common/hooks/useInterval';
-import QuickPick from '../common/QuickPick';
-/**
- * Renders a date with a edit icon
- * @param {Object} props
- * @param {Date} props.startDate
- */
-function Started({ startDate }) {
-  const formattedDate = format(startDate, 'E. HH:mm');
 
-  return (
-    <Box>
-      <Typography variant="h6">Started:</Typography>
-      <Box display="flex" flexDirection="row" alignItems="flex-start">
-        <Box fontSize="1.125rem" lineHeight="1.125rem" padding="3px" clone>
-          <Typography variant="subtitle1">{formattedDate}</Typography>
-        </Box>
-        <IconButton size="small">
-          <Box fontSize="1.125rem" clone>
-            <EditIcon />
-          </Box>
-        </IconButton>
-      </Box>
-    </Box>
-  );
-}
+import QuickPick from '../common/QuickPick';
+import useAppSelector from 'hooks/useSelector';
+import selectDefinition from 'features/session-definition/selectDefinition';
 
 const addMinutesToSession = (amount, edit, runningSession) => () =>
   edit({
@@ -53,6 +27,9 @@ export default function TimerTab() {
   const { runningSession, editRunningSession } = useEditRunningSession();
   const { stopRunningSession } = useStopRunningSession();
   const runningStats = useRunningSessionStats();
+  const sessionDefinition = useAppSelector(state =>
+    selectDefinition(state, { name: runningSession?.name }),
+  );
   // Tick every minute to update the UI
   const [count, setCount] = React.useState(0);
   useInterval(() => {
@@ -62,7 +39,7 @@ export default function TimerTab() {
   const resetSession = () =>
     editRunningSession({ name: runningSession?.name, startDate: new Date() });
   const add5min = addMinutesToSession(5, editRunningSession, runningSession);
-  const add30min = addMinutesToSession(5, editRunningSession, runningSession);
+  const add30min = addMinutesToSession(30, editRunningSession, runningSession);
   const remove5min = addMinutesToSession(-5, editRunningSession, runningSession);
   const remove30min = addMinutesToSession(-30, editRunningSession, runningSession);
 
@@ -77,48 +54,41 @@ export default function TimerTab() {
   const name = runningSession.name;
   return (
     <Page>
-      <Box
-        display="flex"
-        pt={4}
-        pb={2}
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        fontSize="2rem"
-        fontFamily="'Roboto',Monospace"
-      >
-        <RenderTimer startDate={dateStart.toISOString()} />
-        <Typography>{name}</Typography>
-      </Box>
+      <Card>
+        <CircularTimer
+          name={name}
+          startDate={dateStart}
+          color={sessionDefinition?.color}
+          expectedDuration={sessionDefinition?.expectedDuration || 60}
+        />
+      </Card>
       <Box
         display="flex"
         flexDirection="row"
         alignItems="flex-start"
         justifyContent="space-between"
-      >
-        <Started startDate={dateStart} />
-      </Box>
+      ></Box>
       <Box display="flex" justifyContent="center" paddingBottom={2} paddingTop={1}>
         <StatRow title="Today" subtitle={msToHuman(runningStats.today)} />
         <StatRow title="Week" subtitle={msToHuman(runningStats.thisWeek)} />
         <StatRow title="Month" subtitle={msToHuman(runningStats.thisMonth)} />
       </Box>
-      <Grid container spacing={1} alignContent="center" alignItems="center" justify="center">
-        <ButtonActionText onClick={add5min} icon={<Add />} text='+5"' />
-        <ButtonActionText onClick={add30min} icon={<Add />} text='+30"' />
-        <ButtonActionText onClick={remove30min} icon={<Remove />} text='-30"' />
-        <ButtonActionText onClick={remove5min} icon={<Remove />} text='-5"' />
-      </Grid>
-      <Box display="flex" justifyContent="center" alignItems="center" p={1}>
-        <Box paddingRight={1}>
-          <ButtonAction onClick={resetSession} icon={<Replay />} />
+      <Card>
+        <Box display="flex" alignItems="center" justifyContent="center" p={1}>
+          <ButtonActionText onClick={add5min} icon={<Add />} text='+5"' />
+          <ButtonActionText onClick={add30min} icon={<Add />} text='+30"' />
+          <ButtonActionText onClick={remove30min} icon={<Remove />} text='-30"' />
+          <ButtonActionText onClick={remove5min} icon={<Remove />} text='-5"' />
         </Box>
-        <ButtonAction
-          icon={<StopIcon />}
-          color="secondary"
-          onClick={stopRunningSession}
-        ></ButtonAction>
-      </Box>
+        <Box display="flex" justifyContent="center" alignItems="center" p={1}>
+          <Box padding={1}>
+            <ButtonAction onClick={resetSession} icon={<Replay />} />
+          </Box>
+          <Box padding={1}>
+            <ButtonAction icon={<StopIcon />} color="secondary" onClick={stopRunningSession} />
+          </Box>
+        </Box>
+      </Card>
     </Page>
   );
 }
@@ -161,7 +131,13 @@ function StatRow({ title, subtitle }) {
  **/
 function ButtonAction({ onClick, icon, color = 'primary' }) {
   return (
-    <Button variant="outlined" color={color} onClick={onClick} size="large">
+    <Button
+      variant="outlined"
+      color={color}
+      onClick={onClick}
+      size="large"
+      style={{ width: '62px' }}
+    >
       <Box fontSize="2rem" clone>
         {icon}
       </Box>
@@ -185,6 +161,7 @@ function ButtonActionText({ onClick, icon, text, color = 'primary' }) {
         color={color}
         onClick={onClick}
         size="large"
+        style={{ width: '62px' }}
         // startIcon={icon}
       >
         <Box>{text}</Box>
