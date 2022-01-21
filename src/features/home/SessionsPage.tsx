b2750b81c1de694c90c154a834315078ea92a053
Danielo Rodriguez <rdanielo@gmail.com>
2022-01-21 17:39:02 +0100

    feat: button to create new definition

--- src/features/home/SessionsPage.tsx
+++ src/features/home/SessionsPage.tsx
@@ -8,13 +8,19 @@ import EditSession from './EditSession';
 import { FooterWithVersion } from '../common/index';
 import selectGroupedSessions from './redux/selectGroupedSessions';
 import { RootState } from '@common/configStore';
+import Fab from '@mui/material/Fab';
+import { Add } from '@mui/icons-material';
+import { Slide, useTheme } from '@mui/material';
+import selectRunningSession from './redux/selectRunningSession';
+import { Link } from 'react-router-dom';
 
 const SessionsPage = (props: import('react-redux').ConnectedProps<typeof connector>) => {
   useEffect(() => {
     const { fetchSessions } = props;
     fetchSessions();
   }, []);
-  const { switchTask, editSession, groupedSessions } = props;
+  const { switchTask, editSession, groupedSessions, runningSession } = props;
+  const theme = useTheme();
   return (
     <div className="home-default-page">
       <SessionsList
@@ -24,7 +30,18 @@ const SessionsPage = (props: import('react-redux').ConnectedProps<typeof connect
       />
       <EditSession />
       <SessionController />
-      <Box pt={2} pb={1} className="home-copyright">
+      <Slide in={!runningSession}>
+        <Fab
+          color="secondary"
+          component={Link}
+          to="/session-definitions/new"
+          aria-label="create-new-session"
+          sx={{ right: theme.spacing(2), bottom: theme.spacing(2), position: 'absolute' }}
+        >
+          <Add />
+        </Fab>
+      </Slide>
+      <Box pt={3} pb={1} className="home-copyright">
         <FooterWithVersion />
       </Box>
     </div>
@@ -34,6 +51,7 @@ const SessionsPage = (props: import('react-redux').ConnectedProps<typeof connect
 function mapStateToProps(state: RootState) {
   return {
     groupedSessions: selectGroupedSessions(state),
+    runningSession: selectRunningSession(state),
   };
 }
 
